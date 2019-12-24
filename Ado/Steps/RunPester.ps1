d8d4b019fbb5b70ac5cfbe176828f62e16591809
James Brundage <james.brundage@start-automating.com>
2019-12-24 12:39:34 -0800

    Updating Azure DevOps Pipeline Step to provide rich errors if PSDevOps is installed.

--- Ado/Steps/RunPester.ps1
+++ Ado/Steps/RunPester.ps1
@@ -3,6 +3,24 @@ Import-Module ".\$moduleName.psd1" -Force -PassThru | Out-Host
 $result = 
     Invoke-Pester -PassThru -Verbose -OutputFile ".\$moduleName.TestResults.xml" -OutputFormat NUnitXml `
         -CodeCoverage "$(Build.SourcesDirectory)\*-*.ps1" -CodeCoverageOutputFile ".\$moduleName.Coverage.xml"
-if ($result.FailedCount -gt 0) {
-    throw "$($result.FailedCount) tests failed."
+
+$psDevOpsImported = Import-Module PSDevOps -Force -PassThru -ErrorAction SilentlyContinue
+
+if ($psDevOpsImported) {
+    foreach ($pesterTestResult in $pesterResults.TestResult) {
+        if ($pesterTestResult.Result -eq 'Failed') {
+            $foundLineNumber = [Regex]::Match($pesterTestResult.StackTrace, ':\s{0,}(?<Line>\d+)\s{0,}\w{1,}\s{0,}(?<File>.+)$', 'Multiline')
+            $errSplat = @{
+                Message = $pesterTestResult.ErrorRecord.Exception.Message
+                Line = $foundLineNumber.Groups["Line"].Value
+                SourcePath = $foundLineNumber.Groups["File"].Value
+            }
+
+            Write-ADOError @errSplat
+        }    
+    }    
+} else {
+    if ($result.FailedCount -gt 0) {
+        throw "$($result.FailedCount) tests failed."
+    }
 }
