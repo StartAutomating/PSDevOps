c9e6e8261c0f3f7cc7e7d7f4e54662f4e29855db
James Brundage <james.brundage@start-automating.com>
2019-12-24 12:38:36 -0800

    Allowing Special Values in Nested Hashtables, and allowing non-hashtable values in a .PSD1

--- Parts/ExpandComponents.ps1
+++ Parts/ExpandComponents.ps1
@@ -42,7 +42,13 @@ $splatMe.Remove('PartTable')
         $metaData = $theComponentMetaData["$thingType.$v"]
         $ft = if ($metaData.Path) { [IO.File]::ReadAllText($metaData.Path) }
         if ($propName -eq $thingType -and -not $singleton) {
-            $v; continue nextValue
+            if ($v -is [Collections.IDictionary]) {
+                & $ExpandComponents $v @splatMe
+            } else {
+                $v
+            }
+            
+            continue nextValue
         }
 
 
@@ -76,7 +82,11 @@ $splatMe.Remove('PartTable')
                 }
                 $data = & ([ScriptBlock]::Create(($ft -replace '@{', '[Ordered]@{')))
                 $splatMe.Parent = $partTable
-                & $ExpandComponents $data @splatMe
+                if ($data -is [Collections.IDictionary]) {
+                    & $ExpandComponents $data @splatMe
+                } else {
+                    $data
+                }
             }
             elseif ($metaData.Extension -eq '.sh') {
                 $out = [Ordered]@{bash="$ft";displayName=$metaData.Name}
