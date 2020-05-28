b9ef7158f1e309c9c156fa18c3d7729f16bf729e
James Brundage <james.brundage@start-automating.com>
2020-05-28 11:41:49 -0700

    Changing default to 4.99.99.  Adding -SkipPublisherCheck and -AllowClobber.

--- Ado/Steps/InstallPester.ps1
+++ Ado/Steps/InstallPester.ps1
@@ -1,7 +1,7 @@
 ﻿param(
 [string]
-$PesterMaxVersion = '4.9.9'
+$PesterMaxVersion = '4.99.99'
 )
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Install-Module -Name Pester -Repository PSGallery -Force -Scope CurrentUser -MaximumVersion $PesterMaxVersion 
+Install-Module -Name Pester -Repository PSGallery -Force -Scope CurrentUser -MaximumVersion $PesterMaxVersion -SkipPublisherCheck -AllowClobber
 Import-Module Pester -Force -PassThru -MaximumVersion $PesterMaxVersion
