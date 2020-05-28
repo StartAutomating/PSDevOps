f6004d91a23682d40a7102fe64cb36fe26d928c1
James Brundage <james.brundage@start-automating.com>
2020-05-28 11:15:36 -0700

    Adding -MaximumVersion to Import-Module.  Using TLS 1.2.

--- Ado/Steps/InstallPester.ps1
+++ Ado/Steps/InstallPester.ps1
@@ -2,5 +2,6 @@
 [string]
 $PesterMaxVersion = '4.9.9'
 )
-Install-Module -Name Pester -Repository PSGallery -Force -Scope CurrentUser -MaximumVersion $PesterMaxVersion
-Import-Module Pester -Force -PassThru
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Install-Module -Name Pester -Repository PSGallery -Force -Scope CurrentUser -MaximumVersion $PesterMaxVersion 
+Import-Module Pester -Force -PassThru -MaximumVersion $PesterMaxVersion
