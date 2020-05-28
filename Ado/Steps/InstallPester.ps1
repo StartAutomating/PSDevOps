5463eaca35e48299c07677fc0767065aa573cf52
James Brundage <james.brundage@start-automating.com>
2020-05-28 10:53:25 -0700

    Adding -PesterMaxVersion to InstallPester step.  Defaulting to 4.9.9.

--- Ado/Steps/InstallPester.ps1
+++ Ado/Steps/InstallPester.ps1
@@ -1,2 +1,6 @@
-﻿Install-Module -Name Pester -Repository PSGallery -Force -Scope CurrentUser
+﻿param(
+[string]
+$PesterMaxVersion = '4.9.9'
+)
+Install-Module -Name Pester -Repository PSGallery -Force -Scope CurrentUser -MaximumVersion $PesterMaxVersion
 Import-Module Pester -Force -PassThru
