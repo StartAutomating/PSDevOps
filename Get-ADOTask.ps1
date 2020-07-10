dfa36ed150aa4b6a8b94f18cb34902a117dd84f2
James Brundage <james.brundage@start-automating.com>
2020-07-09 21:20:48 -0700

    Removing -ApiVersion parameter from Get-ADOTask (TFS will not support it, and Azure DevOps will ignore it)

--- Get-ADOTask.ps1
+++ Get-ADOTask.ps1
@@ -7,8 +7,11 @@
         Gets Tasks and Task Groups from Azure DevOps
     .Example
         Get-ADOTask -Organization StartAutomating
+    .Link
+        Convert-ADOPipeline
     #>
     [CmdletBinding(DefaultParameterSetName='/{Organization}/_apis/distributedTask/Tasks')]
+    [OutputType('PSDevOps.Task')]
     param(
     # The organization
     [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
@@ -17,12 +20,14 @@
     $Organization,
 
     # The project.  Required to get task groups.
-    [Parameter(Mandatory,ValueFromPipelineByPropertyName,ParameterSetName='/{Organization}/{Project}/_apis/distributedTask/taskGroups/')]
+    [Parameter(Mandatory,ValueFromPipelineByPropertyName,
+        ParameterSetName='/{Organization}/{Project}/_apis/distributedTask/taskGroups/')]
     [string]
     $Project,
 
     # If set, will get task groups related to a project.
-    [Parameter(Mandatory,ValueFromPipelineByPropertyName,ParameterSetName='/{Organization}/{Project}/_apis/distributedTask/taskGroups/')]
+    [Parameter(Mandatory,ValueFromPipelineByPropertyName,
+        ParameterSetName='/{Organization}/{Project}/_apis/distributedTask/taskGroups/')]
     [Alias('TaskGroups')]
     [switch]
     $TaskGroup,
@@ -31,13 +36,7 @@
     # To use against TFS, provide the tfs server URL (e.g. http://tfsserver:8080/tfs).
     [Parameter(ValueFromPipelineByPropertyName)]
     [uri]
-    $Server = "https://dev.azure.com/",
-
-    # The api version.  By default, 5.1.
-    # If targeting TFS, this will need to change to match your server version.
-    # See: https://docs.microsoft.com/en-us/azure/devops/integrate/concepts/rest-api-versioning?view=azure-devops
-    [string]
-    $ApiVersion = "5.1-preview"
+    $Server = "https://dev.azure.com/"
     )
 
     dynamicParam { . $GetInvokeParameters -DynamicParameter }
@@ -49,26 +48,12 @@
     }
 
     process {
-        $psParameterSet = $psCmdlet.ParameterSetName
-
-
         $uri = # The URI is comprised of:
             @(
                 "$server".TrimEnd('/')   # the Server (minus any trailing slashes),
-                (. $ReplaceRouteParameter $psParameterSet)
+                (. $ReplaceRouteParameter $psCmdlet.ParameterSetName)
                                          # and any parameterized URLs in this parameter set.
             ) -as [string[]] -ne '' -join '/'
-
-        $uri += '?' # The URI has a query string containing:
-        $uri += @(
-            if ($Server -ne 'https://dev.azure.com/' -and
-                -not $PSBoundParameters.ApiVersion) {
-                $ApiVersion = '2.0'
-            }
-            if ($ApiVersion) { # the api-version
-                "api-version=$apiVersion"
-            }
-        ) -join '&'
 
         # We want to decorate our return value.  Handily enough, both URIs contain a distinct name in the last URL segment.
         $typename = @($psCmdlet.ParameterSetName -split '/')[-1].TrimEnd('s') # We just need to drop the 's'
@@ -79,10 +64,10 @@
         )
 
         $invokeResult = Invoke-ADORestAPI -Uri $uri @invokeParams
-        if ($invokeResult -is [string]) {
-            $invokeResult = ($invokeResult -replace '""', '"_blank"') |
-                Microsoft.PowerShell.Utility\ConvertFrom-Json |
-                Select-Object -ExpandProperty Value
+        if ($invokeResult -is [string]) { # The /tasks endpoint returns malformed JSON.
+            $invokeResult = ($invokeResult -replace '""', '"_blank"') | # we have to fix it, by replacing blank strings with "_blank"
+                Microsoft.PowerShell.Utility\ConvertFrom-Json | # then we use the built-in ConvertFrom-JSON
+                Select-Object -ExpandProperty Value # and expand out the .Value property.
             $invokeResult = foreach ($ir in $invokeResult) {
                 $ir.pstypenames.clear()
                 foreach ($tn in $typeNames) {
@@ -92,8 +77,8 @@
             }
         }
 
-        $invokeResult |
-            Add-Member NoteProperty Organization $Organization -Force -PassThru |
-            Add-Member NoteProperty Server $Server -Force -PassThru
+        $invokeResult | # Now that we've fixed the result
+            Add-Member NoteProperty Organization $Organization -Force -PassThru | # add on the .Organization property
+            Add-Member NoteProperty Server $Server -Force -PassThru # and the .Server property.
     }
 }
