c694a34b7fd226046e247b9167ba9ca26bdb1056
James Brundage <james.brundage@start-automating.com>
2020-07-20 20:51:09 -0700

    Adding Write-GitHubOutput.

--- Write-GitHubOutput.ps1
+++ Write-GitHubOutput.ps1
@@ -11,6 +11,8 @@
         Write-GitHubOutput @{
             key = 'value'
         }
+    .Example
+        Get-Random -Minimum 1 -Maximum 10 | Write-GitHubOutput -Name RandomNumber
     .Link
         Write-GitHubError
     #>
@@ -34,7 +36,7 @@
         $inQ    = [Collections.Queue]::new()
     }
     process {
-        #region Enqueue Input
+        #region Output Dictionaries
         if ($InputObject -is [Collections.IDictionary]) {                
             $gitOut = 
                 foreach ($kv in $InputObject.GetEnumerator()) {
@@ -46,10 +48,40 @@
             } else {
                 $gitOut
             }
-        } else {
+        }
+        #endregion Output Dictionaries
+        #region Output Errors
+        elseif ($InputObject -is [Management.Automation.ErrorRecord]) {
+            $gitHubErrorParams = @{
+                Message = $InputObject.Exception.Message
+            }
+            $stackTraceLine =
+                @($InputObject.ScriptStackTrace -split 'at <ScriptBlock>,' -ne '' -match ':*line')[0]
+            if ($stackTraceLine) {
+                $stackTraceLineParts = @($stackTraceLine -split ':')
+                $gitHubErrorParams.Line = $stackTraceLineParts[-1] -replace 'line' -replace '\s'
+                
+                $file = $stackTraceLineParts[0..($stackTraceLineParts.Count - 2)] -join ':'
+                if ($file -notlike '*<*>*') {
+                    $gitHubErrorParams.File = $file
+                } 
+
+            }
+            Write-GitHubError @gitHubErrorParams            
+        }
+        #endregion Output Errors
+        #region Output Warnings
+        elseif ($InputObject -is [Management.Automation.WarningRecord]) 
+        {
+            Write-GitHubWarning -Message $InputObject.Message
+        }
+        #endregion Output Warnings
+        #region Enqueue Remaining Input
+        else
+        {
             $inQ.Enqueue($InputObject)
         }
-        #endregion Enqueue Input
+        #endregion Enqueue Remaining Input
     }
 
     end {
@@ -59,10 +91,12 @@
                 $inQ.Clear()
             }
 
-        if ($env:GITHUB_WORKFLOW -and $DebugPreference -eq 'SilentlyContinue') {
-            Write-Host ($gitOut -join [Environment]::NewLine)
-        } else {
-            $gitOut
+        if ($gitOut) {
+            if ($env:GITHUB_WORKFLOW -and $DebugPreference -eq 'SilentlyContinue') {
+                Write-Host ($gitOut -join [Environment]::NewLine)
+            } else {
+                $gitOut
+            }
         }
     }
 }
