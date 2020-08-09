b8211051bb42ac32cab4dcf1e4a75ab6ad7a0d77
James Brundage <james.brundage@start-automating.com>
2020-08-09 16:36:19 -0700

    Adding formatting for InstallExtensions and ExtensionContributions.

---
+++ Formatting/PSDevOps.ExtensionContribution.format.ps1
@@ -0,0 +1,6 @@
+﻿Write-FormatView -TypeName PSDevOps.ExtensionContribution -Action {
+    Write-FormatViewExpression -Text 'ContributionID: '
+    Write-FormatViewExpression -ScriptBlock { $_.ContributionID +[Environment]::NewLine }
+    Write-FormatViewExpression -If { $_.Name } -ScriptBlock { '  ' + $_.Name } 
+    Write-FormatViewExpression -If { $_.Description } -ScriptBlock { [Environment]::NewLine + '    ' + $_.Description }
+} # -GroupByProperty ContributionID -GroupLabel ContributionID
