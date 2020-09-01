468b657cc08d7ae8a6996dd9ab255ef1a1e0fad0
James Brundage <james.brundage@start-automating.com>
2020-08-31 18:37:49 -0700

    Adding Connect/Disconnect-ADO

---
+++ Connect-ADO.ps1
@@ -0,0 +1,91 @@
+﻿function Connect-ADO
+{
+    <#
+    .Synopsis
+        Connects to Azure DeVOps
+    .Description
+        Connects the current PowerShell session to Azure DeVOps or a Team Foundation Server.
+
+        Information passed to Connect-ADO will be used as the default parameters to all -ADO* commands from PSDevOps.
+
+        PersonalAccessTokens will be cached separately to improve security.
+    .Example
+        Connect-ADO -Organization StartAutomating -PersonalAccessToken $myPat
+    .Link
+        Disconnect-ADO
+    #>
+    [OutputType('PSDevOps.Connection')]
+    param(
+    # The organization.
+    # When connecting to TFS, this is the Project Collection.
+    [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+    [Alias('Org')]
+    [string]
+    $Organization,
+
+    # The Personal Access Token.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [Alias('PAT')]
+    [string]
+    $PersonalAccessToken,
+
+    # If set, will use default credentials when connecting.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [switch]
+    $UseDefaultCredentials,
+
+    # The credential used to connect.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [Management.Automation.PSCredential]
+    $Credential,
+
+    # The Server.  If this points to a TFS server, it should be the root TFS url, i.e. http://localhost:8080/tfs
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [uri]
+    $Server
+    )
+
+    begin {
+        $myModuleCommands =
+            @(if ($MyInvocation.MyCommand.ScriptBlock.Module) {
+                  $MyInvocation.MyCommand.ScriptBlock.Module.ExportedCommands.Values
+            })
+    }
+
+    process {
+        if (-not $PSDefaultParameterValues) {
+            Write-Error "PSDefaultParameterValues not found"
+            return
+        }
+
+        $toCache = [Ordered]@{} + $PSBoundParameters
+        $toCache.Remove('PersonalAccessToken')
+        #region Set PSDefaultParameterValues
+        foreach ($cmd in $myModuleCommands) {
+            if ($cmd.Name -notlike '*-ADO*') { continue }
+            foreach ($kv in $toCache.GetEnumerator()) {
+                $global:PSDefaultParameterValues["${cmd}:$($kv.Key)"] = $kv.Value
+            }
+        }
+        #endregion Set PSDefaultParameterValues
+
+        #region Cache PersonalAccessToken
+        if ($PersonalAccessToken) {
+            $Script:CachedPersonalAccessToken = $PersonalAccessToken
+            $getProjects = Get-ADOProject @PSBoundParameters
+            if (-not $getProjects) {
+                Disconnect-ADO
+            }
+        }
+        #endregion Cache PersonalAccessToken
+
+        #region Cache and Output Connection
+        $script:ADOConnectionInfo = $toCache
+        $output = [PSCustomObject]$toCache
+        $output.pstypenames.clear()
+        $output.pstypenames.add("$Organization.Connection")
+        $output.pstypenames.add('PSDevOps.Connection')
+        $output
+        #endregion Cache and Output Connection
+    }
+}
---
+++ Disconnect-ADO.ps1
@@ -0,0 +1,42 @@
+﻿function Disconnect-ADO
+{
+    <#
+    .Synopsis
+        Disconnects from Azure DevOps
+    .Description
+        Disconnects from Azure DevOps, clearing parameter value defaults and cached access tokens.
+    .Example
+        Disconnect-ADO
+    .Link
+        Connect-ADO
+    #>
+    [OutputType([Nullable], [PSObject])]
+    [CmdletBinding(ConfirmImpact='High',SupportsShouldProcess)]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("Test-ForPipelineParameter", "", Justification="No parameters but -WhatIf and -Confirm")]
+    param()
+
+    begin {
+        #region Find Commands
+        $myModuleCommands =
+            @(if ($MyInvocation.MyCommand.ScriptBlock.Module) {
+                  $MyInvocation.MyCommand.ScriptBlock.Module.ExportedCommands.Values
+            })
+        #endregion Find Commands
+    }
+
+    end {
+        if ($WhatIfPreference) { return $script:ADOConnectionInfo }
+        if ($script:ADOConnectionInfo -and
+            $PSCmdlet.ShouldProcess("Disconnect from $($script:ADOConnectionInfo.Organization)")) {
+            #region Clear PSDefaultParameterValues
+            foreach ($cmd in $myModuleCommands) {
+                if ($cmd.Name -notlike '*-ADO*') { continue }
+                foreach ($k in @(@($global:PSDefaultParameterValues.Keys) -like "${cmd}:*")) {
+                    $global:PSDefaultParameterValues.Remove($k)
+                }
+            }
+            #endregion Clear PSDefaultParameterValues
+            $Script:CachedPersonalAccessToken = '' # Clear the cached access token.
+        }
+    }
+}
