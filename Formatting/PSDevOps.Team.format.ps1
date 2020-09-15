add09487e5787eb83a8a8c7fbbb8f73a4ca56a65
James Brundage <james.brundage@start-automating.com>
2020-09-15 10:52:52 -0700

    Adding Formatting for PSDevops.Team (Get-ADOTeam).

---
+++ Formatting/PSDevOps.Team.format.ps1
@@ -0,0 +1,5 @@
+﻿Write-FormatView -TypeName PSDevOps.Team -Property ProjectName, Team, Description
+
+Write-FormatView -TypeName PSDevOps.Team -Name ID -Property ProjectName, ProjectID, Team, TeamID
+
+Write-FormatView -TypeName PSDevOps.Team -Name URL -Property ProjectName, Team, Url -Wrap
