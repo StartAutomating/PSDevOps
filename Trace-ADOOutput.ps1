bed74667c1cd7645756b2dab10e35177dc5c210d
James Brundage <james.brundage@start-automating.com>
2020-09-24 12:44:37 -0700

    Adding Trace-ADOOuput/GitHubOutput.

---
+++ Trace-ADOOutput.ps1
@@ -0,0 +1,56 @@
+﻿function Trace-ADOOutput
+{
+    <#
+    .Synopsis
+        Traces information into Azure DevOps Output
+    .Description
+        Traces information about a command into the output of Azure DevOps.
+    .Example
+        Trace-ADOOutput -Command Get-Process -Parameter @{id=$pid}
+    .Example
+        $myInvocation | Trace-ADOOutput
+    .Link
+        Write-ADOOutput
+    .Link
+        https://docs.microsoft.com/en-us/azure/devops/pipelines/scripts/logging-commands
+    #>
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "", Justification="Directly outputs in certain scenarios")]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("Test-ForUnusableFunction", "", Justification="Directly outputs in certain scenarios")]
+    [OutputType([string])]
+    param(
+    # The command line.
+    [Parameter(Mandatory,ParameterSetName='Command',ValueFromPipelineByPropertyName)]
+    [Alias('MyCommand','Line')]
+    [string]
+    $Command,
+
+    # A dictionary of parameters to the command.
+    [Parameter(ParameterSetName='Command',ValueFromPipelineByPropertyName)]
+    [Alias('Parameters', 'BoundParameters')]
+    [Collections.IDictionary]
+    $Parameter
+    )
+
+    process {
+        if ($Command) {
+            #region Write Command Message
+            $message = (
+                '##[command]' + $Command + ' ' + @(
+                    if ($Parameter) {
+                        foreach ($kv in $Parameter.GetEnumerator()) {
+                            '-' + $kv.Key
+                            $kv.Value
+                        }
+                    }
+                ) -join ' '
+            )
+
+            if ($env:Agent_ID -and $DebugPreference -eq 'SilentlyContinue') {
+                Write-Host -Object $message
+            } else {
+                $message
+            }
+            #endregion Write Command Message
+        }
+    }
+}
---
+++ Trace-GitHubOutput.ps1
@@ -0,0 +1,48 @@
+﻿function Trace-GitHubOutput
+{
+    <#
+    .Synopsis
+        Traces information into GitHub Workflow Output
+    .Description
+        Traces information about a command or invocation into the output of a GitHub workflow.
+    .Example
+        Trace-GitHubOutput -Command Get-Process -Parameter @{id=$pid}
+    .Example
+        $myInvocation | Trace-GitHubOutput
+    .Link
+        Write-GitHubDebug
+    #>
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "", Justification="Directly outputs in certain scenarios")]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("Test-ForUnusableFunction", "", Justification="Directly outputs in certain scenarios")]
+    [OutputType([string])]
+    param(
+    # The command line.
+    [Parameter(Mandatory,ParameterSetName='Command',ValueFromPipelineByPropertyName)]
+    [Alias('MyCommand','Line')]
+    [string]
+    $Command,
+
+    # A dictionary of parameters to the command.
+    [Parameter(ParameterSetName='Command',ValueFromPipelineByPropertyName)]
+    [Alias('Parameters', 'BoundParameters')]
+    [Collections.IDictionary]
+    $Parameter
+    )
+
+    process {
+        if ($Command) {
+            #region Write Debug Message
+            Write-GitHubDebug -Message  (
+                $Command + ' ' + @(
+                    if ($Parameter) {
+                        foreach ($kv in $Parameter.GetEnumerator()) {
+                            '-' + $kv.Key
+                            $kv.Value
+                        }
+                    }
+                ) -join ' '
+            )
+            #endregion Write Debug Message
+        }
+    }
+}
