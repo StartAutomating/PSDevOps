79ea25d87be460c8cf4067412cbb1996def2074e
James Brundage <james.brundage@start-automating.com>
2020-10-05 14:17:40 -0700

    Updating ScriptAnalyzer Step: Adding output variables.

--- Ado/Steps/RunPSScriptAnalyzer.ps1
+++ Ado/Steps/RunPSScriptAnalyzer.ps1
@@ -8,8 +8,11 @@ $invokeScriptAnalyzerSplat = @{Path=$ModulePath}
 if ($ENV:PSScriptAnalyzer_Recurse -or $Recurse) {
     $invokeScriptAnalyzerSplat.Recurse = $true
 }
-$result = Invoke-ScriptAnalyzer @invokeScriptAnalyzerSplat
+$result = @(Invoke-ScriptAnalyzer @invokeScriptAnalyzerSplat)
+$violatedRules = $result | Select-Object -ExpandProperty RuleName
 
+Write-ADOVariable -Name PSScriptAnalyzerIssueCount -Value $result.Length -IsOutput
+Write-ADOVariable -Name PSScriptAnalyzerRulesViolated -Value ($violatedRules -join ',') -IsOutput
 foreach ($r in $result) {
     if ('information', 'warning' -contains $r.Severity) {
         Write-ADOWarning -Message "$($r.RuleName) : $($r.Message)" -SourcePath $r.ScriptPath -LineNumber $r.Line -ColumnNumber $r.Column
