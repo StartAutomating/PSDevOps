099d7532ce31c9cb4b4915f4d75c3d4fb540f740
James Brundage <james.brundage@start-automating.com>
2020-10-05 12:46:06 -0700

    Adding Pipeline Definition file.

---
+++ PSDevOps.Pipeline.PSDevOps.ps1
@@ -0,0 +1,8 @@
+﻿New-ADOPipeline -Stage PowerShellStaticAnalysis, TestPowerShellCrossPlatform, UpdatePowerShellGallery -Option @{
+    'TestPowerShellCrossPlatform'=@{
+        env=@{
+            'SYSTEM_ACCESSTOKEN'='$(SYSTEM.ACCESSTOKEN)'
+        }
+    }
+} |
+    Set-Content .\azure-pipelines.yml -Encoding UTF8
