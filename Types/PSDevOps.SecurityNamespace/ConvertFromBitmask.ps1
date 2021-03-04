adf85bfe61e63cb73adb65a820ce5c86eabe1a35
James Brundage <james.brundage@start-automating.com>
2021-03-04 13:34:11 -0800

    Adding types/formatting defintions for PSDevOps.SecurityNamespace

---
+++ Types/PSDevOps.SecurityNamespace/ConvertFromBitmask.ps1
@@ -0,0 +1,13 @@
+﻿param(
+[int]
+$Bitmask
+)
+
+
+@(foreach ($act in $this.Actions) {
+    if ($Bitmask -band $act.bit) {
+        $act.Name
+    }    
+})
+
+
---
+++ Types/PSDevOps.SecurityNamespace/ConvertToBitmask.ps1
@@ -0,0 +1,16 @@
+﻿param(
+[Parameter(Mandatory,Position=0)]
+[string[]]
+$Permission
+)
+
+$bitMask = 0
+
+foreach ($act in $this.Actions) {
+    foreach ($p in $Permission) {
+        if ($act.Name -like $p -or $act.DisplayName -like $p) {
+            $bitMask = $bitmask -bor $act.bit
+        }
+    }
+}
+$bitMask
