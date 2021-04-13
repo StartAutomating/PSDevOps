3d12beb47c7ba50b2257f7e24ae511e6d992fc77
James Brundage <james.brundage@start-automating.com>
2021-04-12 20:39:48 -0700

    Adding Connect/Disconnect-GitHub

---
+++ Disconnect-GitHub.ps1
@@ -0,0 +1,34 @@
+﻿function Disconnect-GitHub
+{
+    <#
+    .Synopsis
+        Disconnects from GitHub
+    .Description
+        Disconnects from GitHub.
+
+        This unloads any dynamically imported commands and clears the cached PersonalAccessToken.
+    .Example
+        Disconnect-GitHub
+    .Link
+        Connect-GitHub
+    #>
+
+    param()
+
+    begin {
+        $dynamicModuleName = "PSDevOps.DynamicGitHub"
+    }
+
+    process {
+        $script:CachedGitPAT = ''
+
+        $loadedModuleNames = Get-Module | Select-Object -ExpandProperty Name
+        if ($loadedModuleNames -contains $dynamicModuleName) {
+            Remove-Module $dynamicModuleName
+        }
+
+        foreach ($k in @($global:PSDefaultParameterValues.Keys)) {
+            if ($k -like "*github*") { $global:PSDefaultParameterValues.Remove($k) }
+        }
+    }
+}
