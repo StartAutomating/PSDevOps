1433351af85bbaec39117895f6d04265a2fc68be
James Brundage <james.brundage@start-automating.com>
2021-04-12 20:54:43 -0700

    Initial Git Format/Types definitions

---
+++ Formatting/PSDevOps.GitRepo.format.ps1
@@ -0,0 +1,5 @@
+﻿Write-FormatView -TypeName PSDevOps.GitRepo -Property Name, '*', '!', Description  -VirtualProperty @{
+    '*' = {$_.'stargazers_count'}
+    '!' = {$_.'open_issues_count'}
+    '/' = {$_.'forks_count'}
+} -Wrap  -Width 30,-5, 5, 5, 0 -GroupByProperty OwnerName
