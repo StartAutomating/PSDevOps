f869e09107528a60ef89b6340ac17a4eae0086cd
James Brundage <james.brundage@start-automating.com>
2021-04-15 14:57:31 -0700

    Updating formatting for TestSuite

--- Formatting/PSDevOps.TestSuite.format.ps1
+++ Formatting/PSDevOps.TestSuite.format.ps1
@@ -1,1 +1,5 @@
-﻿Write-FormatView -TypeName PSDevOps.TestSuite -Property Name, TestSuiteID, LastUpdated, QueryString -Width 20, 12, 12, 0 -Wrap -GroupByProperty TestPlanID
+﻿Write-FormatView -TypeName PSDevOps.TestSuite -Property Name, TestSuiteID, LastUpdated, QueryString -Width 20, 12, 20, 0 -Wrap -GroupByProperty TestPlanName -VirtualProperty @{
+    LastUpdated = {
+        $_.LastUpdated.ToString('s')
+    }
+}
