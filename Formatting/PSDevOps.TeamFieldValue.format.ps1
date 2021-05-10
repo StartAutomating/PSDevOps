7d4630913894e3d24136ef4c357b85673724c92c
James Brundage <james.brundage@start-automating.com>
2021-05-10 14:39:43 -0700

    Adding format definition for TeamFieldValue

---
+++ Formatting/PSDevOps.TeamFieldValue.format.ps1
@@ -0,0 +1,14 @@
+﻿Write-FormatView -TypeName PSDevOps.TeamFieldValue -Property Field, DefaultValue, Values  -VirtualProperty @{
+    Values = {
+            foreach ($v in $_.values) {
+                if ($v.IncludeChildren) { 
+                    "$($v.value)/*"
+                } else {
+                    "$($v.value)"
+                }
+            }
+    }    
+    Field = {
+        $_.field.ReferenceName
+    }
+} 
