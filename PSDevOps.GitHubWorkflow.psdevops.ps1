ebf2cc50dbab3dfe688afb7e602de8d191b4d42e
James Brundage <james.brundage@start-automating.com>
2021-09-27 22:35:21 -0700

    Updating Workflow Defintion File

--- PSDevOps.GitHubWorkflow.psdevops.ps1
+++ PSDevOps.GitHubWorkflow.psdevops.ps1
@@ -1,3 +1,6 @@
 ﻿#requires -Module PSDevOps
-New-GitHubWorkflow -Name "Analyze, Test, Tag, and Publish" -On Push, PullRequest, Demand -Job PowerShellStaticAnalysis, TestPowerShellOnLinux, TagReleaseAndPublish |
+New-GitHubWorkflow -Name "Analyze, Test, Tag, and Publish" -On Push, PullRequest, Demand -Job PowerShellStaticAnalysis, TestPowerShellOnLinux, TagReleaseAndPublish -Environment @{
+    SYSTEM_ACCESSTOKEN = '${{ secrets.AZUREDEVOPSPAT }}'
+    NoCoverage = $true
+}|
     Set-Content .\.github\workflows\TestAndPublish.yml -Encoding UTF8 -PassThru
