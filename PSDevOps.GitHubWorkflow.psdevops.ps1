390a4596a8c495d562b5a7b93138764143924762
James Brundage <james.brundage@start-automating.com>
2021-09-27 22:10:08 -0700

    Adding Workflow Defintion file

---
+++ PSDevOps.GitHubWorkflow.psdevops.ps1
@@ -0,0 +1,3 @@
+﻿#requires -Module PSDevOps
+New-GitHubWorkflow -Name "Analyze, Test, Tag, and Publish" -On Push, PullRequest, Demand -Job PowerShellStaticAnalysis, TestPowerShellOnLinux, TagReleaseAndPublish |
+    Set-Content .\.github\workflows\TestAndPublish.yml -Encoding UTF8 -PassThru
