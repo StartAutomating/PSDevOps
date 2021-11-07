fa5668b0733e003725c4b50cffa506301686f645
James Brundage <james.brundage@start-automating.com>
2021-11-06 17:12:12 -0700

    Adding GitHub Job/Step for Tracing GitHub Events

---
+++ GitHub/Steps/TraceGitHubEvent.ps1
@@ -0,0 +1,8 @@
+﻿# Traces the GitHub Event
+$gitHubEvent = if ($env:GITHUB_EVENT_PATH) {
+    [IO.File]::ReadAllText($env:GITHUB_EVENT_PATH) | ConvertFrom-Json
+} else { $null }
+
+@"
+$($gitHubEvent | ConvertTo-Json -Depth 100)
+"@ | Out-Host
