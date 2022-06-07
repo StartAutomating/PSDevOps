7a7b62bb7ae210df7da4a8d876cfa259a2582d9a
James Brundage <@github.com>
2022-06-07 15:43:19 -0700

    Adding support for HelpOut (#147)

---
+++ PSDevOps.HelpOut.ps1
@@ -0,0 +1,13 @@
+$PSDevOpsLoaded = Get-Module PSDevOps
+if (-not $PSDevOpsLoaded) {
+    $PSDevOpsLoaded = Get-ChildItem -Recurse -Filter "*.psd1" | Where-Object Name -like 'PSDevOps*' | Import-Module -Name { $_.FullName } -Force -PassThru
+}
+if ($PSDevOpsLoaded) {
+    "::notice title=ModuleLoaded::PSDevOps Loaded" | Out-Host
+} else {
+    "::error:: PSDevOps not loaded" |Out-Host
+}
+if ($PSDevOpsLoaded) {
+    Save-MarkdownHelp -Module $PSDevOpsLoaded.Name -PassThru |
+        Add-Member ScriptProperty CommitMessage {"Updating $($this.Name)" } -Force -PassThru
+}
