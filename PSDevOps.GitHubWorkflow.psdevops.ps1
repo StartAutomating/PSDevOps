17e61c0a9c7c80de934ea1e457b01e9f09cb1ae9
James Brundage <+@noreply.github.com>
2024-09-15 12:23:24 -0700

    fix: Updating workflow definition ( Fixes #190 )

--- PSDevOps.GitHubWorkflow.psdevops.ps1
+++ PSDevOps.GitHubWorkflow.psdevops.ps1
@@ -1,14 +1,10 @@
 ﻿#requires -Module PSDevOps
+
 Push-Location $PSScriptRoot
 
 New-GitHubWorkflow -Name "Analyze, Test, Tag, and Publish" -On Push, PullRequest, Demand -Job PowerShellStaticAnalysis, TestPowerShellOnLinux, TagReleaseAndPublish, BuildPSDevOps -Environment @{
     SYSTEM_ACCESSTOKEN = '${{ secrets.AZUREDEVOPSPAT }}'
     NoCoverage = $true
-} -OutputPath .\.github\workflows\TestAndPublish.yml 
-
-Import-BuildStep -ModuleName GitPub
-New-GitHubWorkflow -On Issue, Demand -Job RunGitPub -Name OnIssueChanged |
-    Set-Content (Join-Path $PSScriptRoot .github\workflows\OnIssue.yml) -Encoding UTF8 -PassThru
-
+} -OutputPath .\.github\workflows\TestAndPublish.yml
 
 Pop-Location
